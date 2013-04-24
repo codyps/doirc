96e3f20dc6c1bf2012d66427223fd7422e937092
Cody P Schafer <jmesmon@gmail.com>
2013-04-24 01:15:23 -0700

    ping?

--- Makefile
+++ Makefile
@@ -1,8 +1,14 @@
 all::
 
+ccan: FORCE
+	@$(MAKE) --no-print-directory -C ccan
+dirclean: clean
+	@$(MAKE) --no-print-directory -C ccan clean
+
+test : ccan
 obj-test = main.o penny/tcp.o
 TARGETS = test
-CFLAGS += -I.
-LDFLAGS += -lev
+CFLAGS += -I. -Iccan
+LDFLAGS += -lev -lccan -Lccan
 
 include base.mk
--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <penny/tcp.h>
 #include <penny/print.h>
 #include <penny/penny.h>
+#include <ccan/container_of/container_of.h>
 #include <errno.h>
 #include <string.h>
 #include <stdbool.h>
@@ -13,26 +14,17 @@
 struct conn {
 	ev_io w;
 	FILE *f;
+
+	/* things the server could potentially change from what I set them to.
+	 */
+	int mode;
+	const char *nick;
+
+	/* We need these to connect, put them here. */
+	const char *realname;
+	const char *user;
+	const char *pass;
 };
-
-static void conn_cb(EV_P_ ev_io *w, int revents)
-{
-	char buf[2048];
-	ssize_t r = read(w->fd, buf, sizeof(buf));
-	if (r == 0 && errno != EAGAIN) {
-		fprintf(stderr, "read fail: %s\n", strerror(errno));
-		ev_io_stop(EV_A_ w);
-		ev_break(EV_A_ EVBREAK_ALL);
-	}
-
-	if (r == -1) {
-		fprintf(stderr, "read fail: %s\n", strerror(errno));
-		return;
-	}
-	printf("> ");
-	print_bytes_as_cstring(buf, r, stdout);
-	putchar('\n');
-}
 
 static void send_irc_cmd(struct conn *c, char const *str, ...)
 {
@@ -42,17 +34,41 @@ static void send_irc_cmd(struct conn *c, char const *str, ...)
 	vfprintf(c->f, str, va);
 	va_end(va);
 	fputs("\r\n", c->f);
+	fflush(c->f);
 }
 
-static void irc_connect(struct conn *c, char const *pass,
-		char const *nick, char const *user)
+static void conn_cb(EV_P_ ev_io *w, int revents)
 {
-	if (pass)
-		send_irc_cmd(c, "PASS %s", pass);
-	if (nick)
-		send_irc_cmd(c, "NICK %s", nick);
-	if (user)
-		send_irc_cmd(c, "USER %s", user);
+	char buf[2048];
+	struct conn *c = container_of(w, typeof(*c), w);
+	ssize_t r = read(w->fd, buf, sizeof(buf));
+	if (r == 0) {
+		fputs("server closed link, exiting...\n", stdout);
+		ev_io_stop(EV_A_ w);
+		ev_break(EV_A_ EVBREAK_ALL);
+		return;
+	}
+
+	if (r == -1) {
+		fprintf(stderr, "read fail: %s\n", strerror(errno));
+		return;
+	}
+
+	if (r > 4 && !strncmp("PING", buf, r)) {
+		send_irc_cmd(c, "PING");
+	}
+
+	printf("> ");
+	print_bytes_as_cstring(buf, r, stdout);
+	putchar('\n');
+}
+
+static void irc_connect(struct conn *c)
+{
+	if (c->pass)
+		send_irc_cmd(c, "PASS %s", c->pass);
+	send_irc_cmd(c, "NICK %s", c->nick);
+	send_irc_cmd(c, "USER %s %d %s %s", c->user, c->mode, "ignore", c->realname);
 }
 
 int main(int argc, char **argv)
@@ -79,10 +95,16 @@ int main(int argc, char **argv)
 
 	struct conn conn;
 	conn.f = fdopen(fd, "a");
+	conn.nick = "bye555";
+	conn.mode = 0;
+	conn.user = "bye555";
+	conn.realname = "bye555";
+	conn.pass = NULL;
+
 	ev_io_init(&conn.w, conn_cb, fd, EV_READ);
 	ev_io_start(EV_DEFAULT, &conn.w);
 
-	irc_connect(&conn, "hi", "bye_555", "sigh");
+	irc_connect(&conn);
 
 	ev_run(EV_DEFAULT, 0);
 	return 0;
