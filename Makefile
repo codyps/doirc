0d1656f07a338118c29988d7f7d1ec0d66983a72
Cody P Schafer <devel@codyps.com>
2014-01-26 01:56:36 -0800

    update base.mk

--- Makefile
+++ Makefile
@@ -1,14 +1,10 @@
 all::
 
-ccan: FORCE
-	@$(MAKE) $(MAKEFLAGS) $(MAKE_ENV) LD="ld" --no-print-directory -C ccan
-dirclean: clean
-	@$(MAKE) $(MAKEFLAGS) $(MAKE_ENV) --no-print-directory -C ccan clean
-
 test : ccan
 obj-test = main.o irc.o penny/debug.o rbtree/rbtree.o
 TARGETS = test
-CFLAGS += -I. -Iccan
-LDFLAGS += -lev -lccan -Lccan
+ALL_CFLAGS += -I.
+ALL_LDFLAGS += -lev
 
 include base.mk
+include base-ccan.mk
---
+++ base-ccan.mk
@@ -0,0 +1,27 @@
+## base-ccan.mk: dfdb8a2, see https://github.com/jmesmon/trifles.git
+CCAN_CFLAGS ?= $(ALL_CFLAGS)
+# Blah, fix LDFLAGS
+
+ifndef BASE_MK_MANUAL_CCAN
+$(obj-all) : ccan
+ALL_CPPFLAGS += -Iccan
+ALL_LDFLAGS  += -Lccan -lccan
+endif
+
+ifndef V
+	QUIET_SUBMAKE  = @ echo '  MAKE ' $@;
+endif
+
+export CCAN_CFLAGS
+export CCAN_LDFLAGS
+
+.PHONY: ccan
+ccan: FORCE
+	$(QUIET_SUBMAKE)$(MAKE) $(MAKE_ENV) --no-print-directory -C $@ $(MAKEFLAGS)
+
+.PHONY: ccan.clean
+ccan.clean :
+	$(QUIET_SUBMAKE)$(MAKE) --no-print-directory -C $(@:.clean=) $(MAKEFLAGS) clean
+
+.PHONY: dirclean
+dirclean : clean ccan.clean
